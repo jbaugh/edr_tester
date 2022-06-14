0f6a3d2fc49121750d866dccb98d5880a38b82ab
Jarrett Baugh <jarrett.baugh@gmail.com>
2022-06-14 11:46:19 -0700

    Work on OS separation

--- src/system/details.rb
+++ src/system/details.rb
@@ -3,7 +3,25 @@ require_relative 'linux'
 module System
   class Details
     def self.new
-      System::Linux.new
+      if os == 'linux'
+        System::Linux.new
+      elsif os == 'windows'
+        System::Windows.new
+      else
+        # shoot for the stars by trying linux
+        System::Linux.new
+      end
+    end
+
+
+    def self.os
+      if RUBY_PLATFORM =~ /cygwin/ || ENV['OS'] == 'Windows_NT'
+        'windows'
+      elsif RbConfig::CONFIG['host_os'].include?('linux')
+        'linux'
+      else
+        'unknown'
+      end
     end
   end
 end
---
+++ src/system/windows.rb
@@ -0,0 +1,23 @@
+module System
+  class Windows
+    def process_start_time(pid)
+      1
+    end
+
+    def process_user(pid)
+      ''
+    end
+
+    def process_name(pid)
+      ''
+    end
+
+    def process_command_line(pid)
+      ''
+    end
+
+    def current_ip_address
+      ''
+    end
+  end
+end
